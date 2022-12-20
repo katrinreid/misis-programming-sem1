3d8698811d3d8e1f2ac99ba7b640410425ee4ec9
Katrin <113970326+EkaterinaRid@users.noreply.github.com>
2022-12-20 13:48:08 +0300

    Update bpm_22_4_2.cpp

--- rid_e_r/bpm_22_4_2.cpp
+++ rid_e_r/bpm_22_4_2.cpp
@@ -4,24 +4,22 @@
 int main() {
     double x = 0, y = 0;
     int yes = 0, no = 0;
-    while(std::cin) {
-        std::cin >> x >> y;
-        double len = std::pow(std::pow(x, 2) + std::pow(y, 2), 0.5);
-        if (len <= 1) {
-            if ((x <= 0 and y >= 0)) {
-                double n_y = x + 1;
-                if (y >= n_y) {
-                    yes += 1;
-                } else {
-                    no += 1;
-                }
-            } else if (x >= 0 and y <= 0) {
-                double n_y = x - 1;
-                if (y <= n_y) {
-                    yes += 1;
-                } else {
-                    no += 1;
-                }
+    while (!std::cin.fail()) {
+        std::cin >> x;
+        std::cin >> y;
+        if ((x <= 0 and y >= 0) and (std::pow(x, 2) + std::pow(y, 2) <= 1)) {
+            double n_y = x + 1;
+            if (y >= n_y) {
+                yes += 1;
+            } else {
+                no += 1;
+            }
+        } else if ((x >= 0 and y <= 0) and (std::pow(x, 2) + std::pow(y, 2) <= 1)) {
+            double n_y = x - 1;
+            if (y <= n_y) {
+                yes += 1;
+            } else {
+                no += 1;
             }
         } else {
             no += 1;
@@ -30,3 +28,8 @@ int main() {
     double k = (yes * 100 / (yes + no));
     std::cout << k * 0.01 << std::endl;
 }
+
+
+// код был верным и до изменения, т.е. решила ещё тогда, но был косяк с выводом ответа
+// оказалось, что command+D надо вводить сразу после последнего символа без нажатия на enter и другие клавиши
+// чтобы понять это, пришлось переписать код, предыдущий код правильный, но этот мне нравится больше
